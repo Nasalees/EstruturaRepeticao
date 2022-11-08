74688749615198c14c94ee040d04c27e2025c305
Nathalia Sales <contatonathaliasales@gmail.com>
2022-11-08 17:56:21 -0300

    One more

---
+++ senha.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de 
+senha incorreta informada, escrever a mensagem "Senha Invalida! Tente novamente:". Quando a senha 
+for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo 
+encerrado. Considere que a senha correta é o valor 2002.*/
+
+int main(){
+    int senha, i;
+
+    printf("Digite a senha");
+    scanf("%d", &senha);
+
+    while (senha != 2002)
+    {
+        i++;
+
+        if(senha != 2002){
+            printf("Senha Invalida! Tente novamente:");
+            scanf("%d", &senha);
+        }
+    }
+    if (senha == 2002){
+        printf("Acesso permitido!");
+    }
+    
+
+    return 0;
+}
